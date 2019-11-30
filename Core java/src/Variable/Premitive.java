a54f0214d585b39e4dfbb246a846ed02daedd839
Akanksha <Akanksha@J96SVQ1-PC>
2019-11-30 11:31:41 +0530

    I am pushing my code

---
+++ Core java/src/Variable/Premitive.java
@@ -0,0 +1,37 @@
+package Variable;
+
+public class Premitive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int integervariable=1200;//-2,147,483,648 to 2,147,483,647
+    System.out.println("Integervarible: " + integervariable);
+    
+    byte bytevariable=15; //-128 to 127
+    System.out.println("Bytevariable: " + bytevariable);
+    
+    float floatvari=1.3f;
+    System.out.println("float Variable: " +floatvari);
+    
+    double doublevari=155.26;
+    System.out.println("Double variable: " +doublevari);
+    
+    short shortvari=16500;//-32,768 to 32767
+    System.out.println("shortvariable: " +shortvari);
+    
+    long longvari=1520000;
+    System.out.println("Long variable: " +longvari);
+    
+    char charvari='a';
+    System.out.println("character variable: " +charvari);
+    
+    boolean booleanvari=true;
+    System.out.println("Boolean Variable: " +booleanvari);
+    
+    
+    
+
+	}
+
+
+}
