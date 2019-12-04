e497799c4d91271a02a7ad295e42f280f6751208
Akanksha <Akanksha@J96SVQ1-PC>
2019-12-04 16:44:22 +0530

    String file

---
+++ Core java/src/Variable/StringDemo.java
@@ -0,0 +1,21 @@
+package Variable;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	String str1= "aks";
+	System.out.println("Hello Akanksha");
+	//compare(), concat(), equals(), split(), length(), replace(), compareTo(), intern(), substring() etc.
+	String str2 = new String("Text Hello");
+	
+	System.out.println("Hello text");
+	
+	System.out.println(str2);
+	
+
+	}
+
+}
+
